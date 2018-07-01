a623ed2ca6d12405a77b262cd12270c1f1d41195
Shoaib Rayeen <shoaib.mca17.du@gmail.com>
2018-07-02 01:06:20 +0530

    Create start_a_new_programming_language.cpp

---
+++ doc/start_a_new_programming_language.cpp
@@ -0,0 +1,18 @@
+---
+layout: page
+title: Start A New Programming Language
+permalink: /doc/start_a_new_programming_language/
+---
+
+Possibly for learning a new programming language , we often think how to start and where to start. The best way to learn anything is by your own.
+The following steps need to follow :
+1. Read the documentation of the language
+2. Install the tool(s) for the language in your laptop/PC
+3. Find some useful links online such as [Tutorial Point](https://www.tutorialspoint.com/)
+4. Read syntax carefully
+5. Read some examples that might give you some idea how to write a program
+6. Practice some basics programs by your own
+
+
+That's one of the ways to learn the programming language. You can do more practice after getting an idea about how to write program and that would be helpful in learning process.
+
